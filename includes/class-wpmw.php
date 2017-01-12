ef6b556a3a129d59cf1c9a825b925111d919c6ed
meiremans <nick@ab-it.io>
2017-01-12 17:00:59 +0200

    create site with name filled in form

--- includes/class-wpmw.php
+++ includes/class-wpmw.php
@@ -174,6 +174,8 @@ class Wpmw {
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
         $this->loader->add_action( 'init', $plugin_public, 'register_shortcodes' );
+        $this->loader->add_action('init',$plugin_public, 'submit_site_name');
+
 
 	}
 
